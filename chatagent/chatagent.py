0bd144b1bfc3bc477b6caf2af6c2d24b61337bc7
klAndersen <darkphoenix@live.no>
2015-11-18 18:04:45 +0100

    Chat Interaction Renamed the file containing the MySQL database class to avoid confusion with the actual driver class. Renamed elements in the html file and updated the CSS to match these changes. Removed the textarea and added back the <div> as chatbox, because textarea cannot format html (e.g. italic, bold, etc). Added JQuery for retrieving the name of the currently active user, for setting the default  welcome message and possibility of sending messages to the Chat Agent.

--- chatagent/chatagent.py
+++ chatagent/chatagent.py
@@ -8,16 +8,13 @@ from xblock.fragment import Fragment
 
 import stackexchange
 
-from mysqldb import MySQLDatabase
+# from mysqldatabase import MySQLDatabase
 
 
 class ChatAgentXBlock(XBlock):
     """
     TO-DO: document what your XBlock does.
     """
-
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -38,6 +35,48 @@ class ChatAgentXBlock(XBlock):
         return frag
 
     @XBlock.json_handler
+    def get_default_welcome_message(self, data, suffix=''):
+        """
+        Get the default welcome message that the user is presented with upon entering the chat.
+
+        Arguments:
+            data
+            suffix (str)
+
+        Returns:
+             JSON: The retrieved welcome message; {'welcome_msg': welcome_msg}
+
+        """
+        # TODO 1: In the master thesis, either retrieve this from DB, or just use this one
+        # TODO 2: Add question to welcome message
+        welcome_msg = "Welcome, "
+        # check that username is set
+        if data['username'] is not None:
+            welcome_msg += "<i>" + data['username'] + "</i>."
+        welcome_msg += "<br />I see that you came from a page with the following Question: <br />"
+        welcome_msg += "<i>&lt;question_displayed_here&gt;</i>"  # TODO 2: insert question here
+        welcome_msg += "<br /> Is this the question you are looking for? Please enter 'Yes' or 'No'."
+        welcome_msg += "<p><br /></p>"
+        return {'welcome_msg': welcome_msg}
+
+    @XBlock.json_handler
+    def get_username(self, data, suffix=''):
+        """
+        Get the name of the currently logged in/active user to display his/her name in the chat agent.
+
+        Arguments:
+            data
+            suffix (str)
+
+        Returns:
+             JSON: The retrieved username; {'username': username}
+
+        """
+        username = "default"
+        # TODO: Retrieve the username of the currently active/logged in user
+        return {'username': username}
+
+    @XBlock.json_handler
     def print_test_data(self, data, suffix=''):
         """
         Just a dummy function to print random content.
@@ -46,8 +85,13 @@ class ChatAgentXBlock(XBlock):
         - Data from StackOverflow
         - ...
 
+        Arguments:
+            data
+            suffix (str)
+
         Returns:
-             JSON: The loaded content
+             JSON: The loaded content; {'result': result}
+
         """
         # return {'result': MySQLDatabase().print_table_content()}
         so = stackexchange.Site(stackexchange.StackOverflow)
--- chatagent/static/js/src/chatagent.js
+++ chatagent/static/js/src/chatagent.js
@@ -1,33 +1,123 @@
 /* Javascript for ChatAgentXBlock. */
 function ChatAgentXBlock(runtime, element) {
+    /**
+    * The name of the currently active user of this application.
+    * The value of USER_NAME is set in the function `getAndSetUsername`.
+    * Marked as const, because it is not intended for this to be changed (except in `getAndSetUsername`).
+    * @see #getAndSetUsername
+    * @const
+    */
+    var USER_NAME = "";
+    /**
+    * HTML newline used in the chatbox
+    * @const
+    */
+    var HTML_NEWLINE = "<p />";
+    /**
+    * The name of the Chat Agent, with HTML formatting for display in the chatbox.
+    * @const
+    */
+    var CHAT_AGENT_NAME = "<strong>Mimmi:</strong> ";
+    /**
+    * Variable for if the welcome message has been displayed yet.
+    */
+    is_welcome_msg_shown = false;
 
-    function updateTextArea(log) {
-        $('.chatbox', element).text(log);
-    }
+    /**
+    * Prints the default welcome message into the chatbox. This should only be called once
+    * (on pageload, or when chatbox is cleared of all content)
+    */
+    function getDefaultWelcomeMessage() {
+        var message = "";
+        username={'username': USER_NAME};
+        console.log(username);
+        invoke('get_default_welcome_message', username, function (data) {
+            message = CHAT_AGENT_NAME + data['welcome_msg'];
+            $('.chatbox').append(message);
+        }); //invoke
+    } //getDefaultWelcomeMessage
 
+    /**
+    * This function retrieves the username from the HTML element in which it were set,
+    * and if the username is not set, it is retrieved by calling the `invoke` function.
+    */
+    function getAndSetUsername() {
+        USER_NAME = $('.username').val();
+        if (USER_NAME == "") {
+            invoke('get_username', null, function (data) {
+                USER_NAME = data['username'];
+                $('.username').text(USER_NAME);
+                //has the welcome message been shown?
+                if (!is_welcome_msg_shown) {
+                    getDefaultWelcomeMessage();
+                } //if
+            }); //invoke
+        } //if
+    } //getAndSetUsername
+
+    /**
+    * Updates the chatbox with the users input and the results from the Chat Agent,
+    * based on the passed input.
+    * @param input {string} User input
+    */
+    function updateChatLog(input) {
+        /*
+            TODO:
+            - Handle and store input (e.g. lookup question, find answers, store in db)
+            - Retrieve results and add to chatbox
+            - Ask user for confirmation (Correct answer: Yes, No, New question)
+        */
+        $('.chatbox').append(input);
+        invoke('print_test_data', input, function (data) {
+            var chatlog = CHAT_AGENT_NAME + data['result'] + HTML_NEWLINE;
+            console.log(chatlog);
+            $('.chatbox').append(chatlog);
+        }); //invoke
+    } //updateChatLog
+
+    /**
+    * Invoke function for retrieving data from the Edx XBlock.
+    * @param method {string} Name of function to call from XBlocks
+    * @param data {object} Data to process || null
+    * @param onSuccess {function (data)} Action to execute on sucessfull POST (e.g. data processing)
+    */
     function invoke(method, data, onSuccess) {
         var handlerUrl = runtime.handlerUrl(element, method);
-
         $.ajax({
             type: "POST",
             url: handlerUrl,
             data: JSON.stringify(data),
             success: onSuccess
-        });
-    }
-
-    invoke('print_test_data', null, function (data) {
-        console.log("log: " + data['result']);
-        $('.chatbox', element).text("Result: " + data['result']);
-    });
+        }); //$.ajax
+    } //invoke
 
     $(function ($) {
-        var outputField = document.getElementById('chatbox');
-        var inputField = document.getElementById('usermsg');
-        inputField.focus();
+        //get the username and set focus in the message field
+        getAndSetUsername();
+        $('#userInput').focus();
 
-        //$('chatbox').text('Hello, user');
+        //exit the chat agent
+        $('#exit').click(function(){
+            var confirmMsg = "Are you sure you want to end the session?";
+            var exit = confirm(confirmMsg);
+                if(exit == true){
+                    //TODO: close window or re-direct user back to course page
+                } //if
+        }); //$('#exit')
 
-    });
+        //send a new message to the chat agent
+        $('#btnSubmit').click(function() {
+            var input = $('#userInput').val();
+            var screenname = USER_NAME + ": ";
+            if (input != "") {
+                input = screenname + $('#userInput').val() + HTML_NEWLINE;
+                updateChatLog(input);
+                $('#userInput').attr('value', "");
+                $('#userInput').focus();
+            } //if
+            return false;
+        }); //$('#btnSubmit')
 
-}
+    }); //$(function ($)
+
+} //ChatAgentXBlock
